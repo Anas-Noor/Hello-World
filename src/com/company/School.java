58ad70ef06a845533b9f56dde05c66c9c62fe349
fahadkhan2001 <fk1271543@gmail.com>
2022-03-24 21:53:36 +0500

    School class added

---
+++ src/com/company/School.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class School {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public School(String name) {
+        this.name = name;
+    }
+}
