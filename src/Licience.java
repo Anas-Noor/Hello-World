af3d195db76b2bcf74613a1100ad97098499a547
Farhan Rana <farhan.rana.3434@gmail.com>
2022-03-24 10:12:39 -0700

    added student class

---
+++ src/Licience.java
@@ -0,0 +1,17 @@
+public class Licience {
+    String name="4210100283";
+    int unit=18;
+
+    public Licience(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Licience{" +
+                "name='" + name + '\'' +
+                ", unit=" + unit +
+                '}';
+    }
+
+}
