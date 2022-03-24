34114c66a6ccbfef831c1cfca8f68b7eeae07bee
Madad <madad.ali5432@gmail.com>
2022-03-24 22:03:44 +0500

    Car Object created and Printed

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,24 +1,48 @@
 package com.company;
 
 public class Car {
-    String name;
+    String car_name;
+    String car_modal;
+    Integer car_price;
 
-    public Car(String name) {
-        this.name = name;
+    public Car(){}
+
+    public Car(String car_name, String car_modal, Integer car_price) {
+        this.car_name = car_name;
+        this.car_modal = car_modal;
+        this.car_price = car_price;
     }
 
-    public String getName() {
-        return name;
+    public String getCar_name() {
+        return car_name;
+    }
+
+    public void setCar_name(String car_name) {
+        this.car_name = car_name;
+    }
+
+    public String getCar_modal() {
+        return car_modal;
+    }
+
+    public void setCar_modal(String car_modal) {
+        this.car_modal = car_modal;
+    }
+
+    public Integer getCar_price() {
+        return car_price;
+    }
+
+    public void setCar_price(Integer car_price) {
+        this.car_price = car_price;
     }
 
     @Override
     public String toString() {
         return "Car{" +
-                "name='" + name + '\'' +
+                "car_name='" + car_name + '\'' +
+                ", car_modal='" + car_modal + '\'' +
+                ", car_price=" + car_price +
                 '}';
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        Car car1 = new Car("Civic","2022",2500000);
+
+        System.out.println(car1);
     }
 }
