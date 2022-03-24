4df0ec05db883b370aeda0436028585df49cbee9
Madad <madad.ali5432@gmail.com>
2022-03-24 23:04:14 +0500

    Change on Car class

--- src/Licience.java
+++ src/Licience.java
@@ -1,6 +1,7 @@
 public class Licience {
     String name="421010028";
     int unit=18;
+    String date= "2022";
 
     public Licience(){
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
 
 
         System.out.println("Changes");
+        System.out.println("asdasdasd");
     }
 }
