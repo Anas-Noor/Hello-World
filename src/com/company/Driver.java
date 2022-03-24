27a321bc5532f5f8538cecebd9ab9bba4b0f9da3
Sheharyar Ahmed <sheriahmed2216@gmail.com>
2022-03-24 22:08:51 +0500

    Driver class

---
+++ src/com/company/Driver.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Driver {
+    String name;
+
+    public Driver(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
